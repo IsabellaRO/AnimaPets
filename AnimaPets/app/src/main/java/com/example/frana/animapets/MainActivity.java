af432c5dad8aa559a7887293bd9c28ec8f92059a
Francisco Ciol Rodrigues Aveiro <fran.aveiro@hotmail.com>
2017-10-30 15:25:30 -0200

    Revert "pagina inicial"

--- AnimaPets/app/src/main/java/com/example/frana/animapets/MainActivity.java
+++ AnimaPets/app/src/main/java/com/example/frana/animapets/MainActivity.java
@@ -1,33 +1,13 @@
 package com.example.frana.animapets;
 
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
-    Spinner spinner;
-    ArrayAdapter<CharSequence> adapter;
-    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        spinner = (Spinner) findViewById(R.id.spinner);
-        adapter = ArrayAdapter.createFromResource(this,R.array.menu_dropdown,android.R.layout.simple_dropdown_item_1line);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        imageView = (ImageView) findViewById(R.id.imagem_pet);
-        spinner.setAdapter(adapter);
-
-        imageView.setImageDrawable(getResources().getDrawable(R.drawable.chiuaua))  ;
-
-
-
     }
 }
