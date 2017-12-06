fda36394b4fdf1e48d5501e11015de28429bbea2
franciol <fran.aveiro@hotmail.com>
2017-12-06 21:54:31 -0200

    imagens redimencionadas, vitrine otimizada e perfis atualizados

--- AnimaPets/app/src/main/java/com/example/frana/animapets/LoginActivity.java
+++ AnimaPets/app/src/main/java/com/example/frana/animapets/LoginActivity.java
@@ -100,14 +100,14 @@ public class LoginActivity extends AppCompatActivity {
 
 
     public void createUsers() {
-        User amanda = new User("Amanda Soares Santos","Rua Ática",500,"","04634042","São Paulo","SP","56782340","991234321","testeamanda@mail.com","63237001406","Max","","03/2014","14","Boxer","Medio","a123456");
-        User joana = new User("Joana da Silva Xavier","Rua Dom Viçoso",60,"","04303001","São Paulo","SP","37462981","991234321","testejoana@mail.com","17855462287",	"Amy","","02/2016","6","lhasa","Pequeno","j123456");
-        User joao = new User("João Paulo Sheffer","Rua Conselheiro Saraiva",600,"apto 306","02037020","São Paulo","SP","59832562","991234321","testejoao@mail.com","34761052295",	"Pablo","","04/2017","9","SRD","Medio","jo123456");
-        User livia = new User("Livia Rubens Paiva","Alameda Santos",741,"apto 801","01419001","São Paulo","SP","58980123","991234321","testelivia@mail.com.br","21099856230",	"Snow","Snow","05/2015","7","Poodle","Pequeno","l123456");
-        User maria = new User("Maria Julia Coelho",	"Avenida Paulista",	1865,	"apto 212",	"01311200",	"São Paulo", 	"SP",	"55667788",	"991234321",	"testemaria@mail.com",	"27384234859",	"Lionel",	"Lion",	"07/2013",	"18",	"Golden Retriever",	"Grande"	,"m123456");
-        User tatiana = new User("Tatiana Castelo Branco","Rua Serra do Japi",340,"","03309000","São Paulo","SP",	"32327788",	"991234321","testetatiana@mail.com"	,"02125086360"	,"Lili","",	"10/2013","6","SRD","Pequeno","t123456");
-        User sabrina = new User("Sabrina de Souza","Rua Eloi Cerqueira",400,"",	"03062010","São Paulo",	"SP",	"33001234"	,"991234321"	,"testesabrina@mail.com"	,"52746478633",	"Tadeu","Tapete","01/2010","12","Chowchow"	,"Medio",	"s123456");
-        User ronei = new User("Ronei Fernandes Fontes","Rua Monte Alegre",502,"","05014000","São Paulo","SP","51231234","991234321","testeronei@mail.com",	"03335401824","Furia","","09/2014",	"15","Bull Terrier",	"Medio",	"r123456");
+        User amanda = new User("Amanda Soares Santos","Rua Ática",500,"","04634042","São Paulo","SP","56782340","991234321","testeamanda@mail.com","63237001406","Max","","03/2014","14","Boxer","Medio","a123456","1");
+        User joana = new User("Joana da Silva Xavier","Rua Dom Viçoso",60,"","04303001","São Paulo","SP","37462981","991234321","testejoana@mail.com","17855462287",	"Amy","","02/2016","6","lhasa","Pequeno","j123456","2");
+        User joao = new User("João Paulo Sheffer","Rua Conselheiro Saraiva",600,"apto 306","02037020","São Paulo","SP","59832562","991234321","testejoao@mail.com","34761052295",	"Pablo","","04/2017","9","SRD","Medio","jo123456","3");
+        User livia = new User("Livia Rubens Paiva","Alameda Santos",741,"apto 801","01419001","São Paulo","SP","58980123","991234321","testelivia@mail.com.br","21099856230",	"Snow","Snow","05/2015","7","Poodle","Pequeno","l123456","4");
+        User maria = new User("Maria Julia Coelho",	"Avenida Paulista",	1865,	"apto 212",	"01311200",	"São Paulo", 	"SP",	"55667788",	"991234321",	"testemaria@mail.com",	"27384234859",	"Lionel",	"Lion",	"07/2013",	"18",	"Golden Retriever",	"Grande"	,"m123456","5");
+        User tatiana = new User("Tatiana Castelo Branco","Rua Serra do Japi",340,"","03309000","São Paulo","SP",	"32327788",	"991234321","testetatiana@mail.com"	,"02125086360"	,"Lili","",	"10/2013","6","SRD","Pequeno","t123456","6");
+        User sabrina = new User("Sabrina de Souza","Rua Eloi Cerqueira",400,"",	"03062010","São Paulo",	"SP",	"33001234"	,"991234321"	,"testesabrina@mail.com"	,"52746478633",	"Tadeu","Tapete","01/2010","12","Chowchow"	,"Medio",	"s123456","7");
+        User ronei = new User("Ronei Fernandes Fontes","Rua Monte Alegre",502,"","05014000","São Paulo","SP","51231234","991234321","testeronei@mail.com",	"03335401824","Furia","","09/2014",	"15","Bull Terrier",	"Medio",	"r123456","8");
         sql.addClasse(amanda);
         sql.addClasse(joana);
         sql.addClasse(joao);
--- AnimaPets/app/src/main/java/com/example/frana/animapets/MainActivity.java
+++ AnimaPets/app/src/main/java/com/example/frana/animapets/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //getWindow().getDecorView().setBackgroundColor(Color.parseColor("#91f58a"));
         getSupportActionBar().hide();
         sql = new SQL(this);
         lhi = (int) getIntent().getSerializableExtra("user");
